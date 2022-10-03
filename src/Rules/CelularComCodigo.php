3a2154a672f948eea9a9a69cf1b4295ad3d7040e
Leandro Viera Marcolin <leandrovmarcolin@gmail.com>
2022-10-03 10:31:40 -0300

    rules celular e telefone

--- src/Rules/CelularComCodigo.php
+++ src/Rules/CelularComCodigo.php
@@ -3,27 +3,23 @@ namespace Bcampti\Larabase\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
 
-/**
- * @author Wallace Maxters <wallacemaxters@gmail.com>
-*/
 class CelularComCodigo implements Rule
 {
-
-    
-    /**
-     * Valida o formato do celular com código do país
-     * 
-     * @param string $attribute
-     * @param string $value
-     * @return boolean
-    */
     public function passes($attribute, $value)
     {
-        return preg_match('/^[+]\d{1,2}\s?\d{2}\s?\d{4,5}\d{4}$/', $value) > 0;
+        if( strlen($value) == 14 ) {
+            $numero = preg_match('/^[+]\d{1,2}\s?\d{2}\s?\d{4,5}\d{4}$/', $value);
+            if( $numero > 0 ){
+                return true;
+            }
+            return false;
+        }else{
+            return preg_match('/^[+]\d{1,2}\s?\(\d{2}\)\s?\d{4,5}\-\d{4}$/', $value) > 0;
+        }
     }
 
     public function message()
     {
-    	return 'O campo :attribute não é um celular com DDD válido.';
+    	return 'O :attribute não é um número válido.';
     }
 }
--- src/Rules/TelefoneComCodigo.php
+++ src/Rules/TelefoneComCodigo.php
@@ -4,28 +4,23 @@ namespace Bcampti\Larabase\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
 
-/**
- * @author Wallace Maxters <wallacemaxters@gmail.com>
- */
 class TelefoneComCodigo implements Rule
 {
-
-    
-    /**
-     * Valida o formato do telefone com código do país
-     * 
-     * @param string $attribute
-     * @param string $value
-     * @return boolean
-     */
     public function passes($attribute, $value)
     {
-        return preg_match('/^[+]\d{1,2}\s?\d{2}\s?\d{4,5}\d{4}$/', $value) > 0;
+        if( strlen($value) == 13 ) {
+            $numero = preg_match('/^[+]\d{1,2}\s?\d{2}\s?\d{4,5}\d{4}$/', $value);
+            if( $numero > 0 ){
+                return true;
+            }
+            return false;
+        }else{
+            return preg_match('/^[+]\d{1,2}\s?\(\d{2}\)\s?\d{4,5}\-\d{4}$/', $value) > 0;
+        }
     }
-
 
     public function message()
     {
-    	return 'O campo :attribute não é um telefone com código e DDD válido.';
+    	return 'O :attribute não é um número válido.';
     }
 }
